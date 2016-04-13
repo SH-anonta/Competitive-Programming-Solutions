f76201ddbd50895202f2e011ca2e7901f6fcfee1
Anonta <anonta9865@gmail.com>
2016-04-13 21:16:16 +0600

    HR entry 3

---
+++ Hacker Rank/Hacker Rank - Dynamic Array.cpp
@@ -0,0 +1,106 @@
+//#Name: Anonta Haque #Problm:  Hacker rank Dynamic Array
+//https://www.hackerrank.com/challenges/dynamic-array
+
+
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdarg>
+#include <utility>
+#include <cctype>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+
+using namespace std;
+
+
+
+typedef long long LL;            //"%lld"
+typedef vector<int> vint;
+typedef pair<int,int> pint;
+typedef unsigned long long ULL;  //"%llu"
+
+
+short COUNT_;
+#define sf scanf
+#define pf printf
+#define NL pf("\n");
+#define put(x_) pf("%d", x_)
+#define read(x_) sf("%d", &x_)
+#define readf(x_) sf("%f", &x_)
+#define SS pf(">_<LOOOOOK@MEEEEEEEEEEEEEEE<<( %d )>>\n",++COUNT_);
+#define DD(x_) cout<<">>>>( "<<++COUNT_<<" ) "<<#x_<<": "<<x_<<endl;
+#define EXT(st_) cout<<"\n>>>Exicution Time: "<<(double)(clock()-st_)/CLOCKS_PER_SEC<<endl;
+
+//constants
+const int SZ= 1E4;
+const int INF= (1<<29);
+const double EPS= 1E-9;
+const double PI= 2*acos(0.0);
+
+//functions templates
+template <typename T>void pary(T* st, T* nd){while(st<nd)cout<<*st++<<" ";NL}
+template <typename T>void rary(T* st, T* nd){while(st<nd)swap(*st++,*--nd);}
+template <typename T>inline void sary(T* st, T* nd){while(st<nd)cin>>*st++;/*sf("%d", st++);*/}
+template <typename T>void tobin(T n,char*bin){int pos=1<<((8*sizeof(T)-2));while((n&pos)==0)pos>>=1;while(pos>=1){if((n & pos)==0)*bin='0';else*bin='1';pos>>=1;bin++;}*bin='\0';}
+template <typename T>inline T LCM(T x,T y){return((x*y)/__gcd(x,y));}
+LL todec(char* bin){LL dec= 0;short i= 0, len= strlen(bin) -1;while(bin[i]){if(bin[i++]=='1'){dec += 1<<len;} len--;}return dec;}
+LL tonum(char* str){LL num= *str++ - '0';while(*str){num= 10*num + (*str++ - '0');}return num;}
+void tostr(LL n,char*st){char*bu=st;while(n>0&&st){*st++='0'+n%10;n/=10;}rary(bu,st);*st='\0';}
+inline LL POW(LL base, int exp){LL p= 1; while(exp-->0){p *= base;} return p;}
+bool ispal(char* str, int len){for(int i= 0; i<len/2; i++){if(str[i]==str[len-i-1]){}else return false;}return true;}
+bool comp(const int& a,const int& b){return a>b;} //reverse sort
+
+
+void solve(void)
+{
+    int n, q, mode, size, lastans= 0;
+    LL x, y, temp;
+ 
+    read(n);
+    read(q);
+    
+    vector<LL>* sqn= new vector<LL>[n];
+    
+    for(int i= 0; i<q; i++)
+    {
+        sf("%d%lld%lld", &mode, &x, &y);
+        
+        if(mode == 1)
+        {
+            sqn[(x^lastans)%n].push_back(y);
+        }
+        else
+        {
+            size= sqn[(x^lastans)%n].size();
+            temp= sqn[(x^lastans)%n][y% size];
+            pf("%lld\n", temp);
+            
+            lastans= temp;
+        }
+       
+    }
+    
+}
+
+
+
+int main(void)
+{
+    solve();
+    return 0;
+    
+}
+
+
+

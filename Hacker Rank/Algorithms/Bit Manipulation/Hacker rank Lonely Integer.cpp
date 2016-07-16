8af93278d044b304ce0498bfa91c0a1653ad9043
SH-anont <anonta9865@gmail.com>
2016-07-16 22:01:16 +0600

    hr entry

---
+++ Hacker Rank/Algorithms/Bit Manipulation/Hacker rank Lonely Integer.cpp
@@ -0,0 +1,78 @@
+//#Name: Anonta Haque #Problm:
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdarg>
+#include <utility>
+#include <bitset>
+#include <cctype>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> vint;
+typedef pair<int,int> pint;
+typedef unsigned long long ULL;
+
+short CC_;
+#define sf scanf
+#define pf printf
+#define NL pf("\n");
+#define PP getchar();
+#define SS printf(">_<LOOOOOK@MEEEEEEEEEEEEEEE<<( %d )>>\n",++CC_);
+#define DD(x_) cout<<">>>>( "<<++CC_<<" ) "<<#x_<<": "<<x_<<endl;
+#define EXT(st_) cout<<"\n>>>Exicution Time: "<<(double)(clock()-st_)/CLOCKS_PER_SEC<<endl;
+
+//constants
+const int SZ= 1E6;
+const int INF= (1<<29);
+const double EPS= 1E-9;
+const double PI= 2*acos(0.0);
+
+void solve(void){
+    int Tc;
+    int n;
+    
+    cin>>n;
+    map<int, int> occ;
+    
+    int temp, lonely;
+    while(n--){
+        cin>>temp;
+        occ[temp]++;
+    }
+    
+    for(pair<int, int> i : occ){
+        if(i.second == 1){
+            cout<< i.first <<endl;
+            break;
+        }
+    }
+    
+    
+}
+
+
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    
+    return 0;
+}

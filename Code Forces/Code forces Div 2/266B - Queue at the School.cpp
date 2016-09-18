ce12f4445412d9635ce391b6d779021cc5aaa3f3
Anonta <anonta9865@gmail.com>
2016-09-19 03:46:40 +0600

    Code forces entry

---
+++ Code Forces/Code forces Div 2/266B - Queue at the School.cpp
@@ -0,0 +1,90 @@
+//#Name: Anonta Haque #Problm:
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdarg>
+#include <utility>
+#include <bitset>
+#include <cctype>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+namespace{
+typedef long long LL;
+typedef vector<int> vint;
+typedef pair<int,int> pint;
+typedef map<int,int> mapii;
+typedef map<int,bool> mapib;
+typedef stringstream strstream;
+typedef unsigned long long ULL;
+
+//Macros
+short CC_;
+#define sf scanf
+#define pf printf
+#define PP getchar();
+#define NL cout<<"\n";
+#define pqueue priority_queue
+#define testb(x_, i_) ((x_&1<<i_)!=0)
+#define setb(x_, i_) (x_=(x_|(1<<i_)))
+#define clrb(x_, i_) (x_=(x_&(!(1<<i_))))
+#define all(container) container.begin(),container.end()
+#define DC(x_) cout<<">>> "<<#x_<<"\n";DA(x_.begin(), x_.end());
+#define DD(x_) cout<<">>>>( "<<++CC_<<" ) "<<#x_<<": "<<x_<<endl;
+#define SS printf(">_<LOOOOOK@MEEEEEEEEEEEEEEE<<( %d )>>\n",++CC_);
+#define EXT(st_) cout<<"\n>>>Exicution Time: "<<(double)(clock()-st_)/CLOCKS_PER_SEC<<endl;
+
+//constants
+const int SZ= 1E6;
+const int INF= (1<<29);
+const double EPS= 1E-9;
+const double PI= 2*acos(0.0);
+const long long MOD= 1000000007;
+
+}
+
+void solve(void){
+    int n,t;
+    string q;
+    
+    cin>>n>>t>>q;
+    
+    for(int i= 0; i<t; i++){
+        for(int j= 0; j<n-1; j++){
+            if(q[j] == 'B' && q[j+1] == 'G'){
+                q[j]= 'G';
+                q[j+1]= 'B';
+                j++;
+            }
+        }
+    }
+    
+    cout<< q <<endl;
+}
+
+
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+   
+
+    
+    return 0;
+}
---
+++ Code Forces/Code forces Div 2/672B - Different is Good.cpp
@@ -0,0 +1,98 @@
+//#Name: Anonta Haque #Problm:
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdarg>
+#include <utility>
+#include <bitset>
+#include <cctype>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+namespace{
+typedef long long LL;
+typedef vector<int> vint;
+typedef pair<int,int> pint;
+typedef map<int,int> mapii;
+typedef map<int,bool> mapib;
+typedef stringstream strstream;
+typedef unsigned long long ULL;
+
+//Macros
+short CC_;
+#define sf scanf
+#define pf printf
+#define PP getchar();
+#define NL cout<<"\n";
+#define pqueue priority_queue
+#define testb(x_, i_) ((x_&1<<i_)!=0)
+#define setb(x_, i_) (x_=(x_|(1<<i_)))
+#define clrb(x_, i_) (x_=(x_&(!(1<<i_))))
+#define all(container) container.begin(),container.end()
+#define DC(x_) cout<<">>> "<<#x_<<"\n";DA(x_.begin(), x_.end());
+#define DD(x_) cout<<">>>>( "<<++CC_<<" ) "<<#x_<<": "<<x_<<endl;
+#define SS printf(">_<LOOOOOK@MEEEEEEEEEEEEEEE<<( %d )>>\n",++CC_);
+#define EXT(st_) cout<<"\n>>>Exicution Time: "<<(double)(clock()-st_)/CLOCKS_PER_SEC<<endl;
+
+//constants
+const int SZ= 1E6;
+const int INF= (1<<29);
+const double EPS= 1E-9;
+const double PI= 2*acos(0.0);
+const long long MOD= 1000000007;
+
+}
+
+void solve(void){
+    int len;
+    string s;    
+    
+    int occur[28];
+    memset(occur, 0, sizeof(int)*27);
+            
+    cin>>len>>s;
+    
+    int sum= 0;
+    if(len > 26){
+        cout<< -1<<endl;
+        return;
+    }
+    
+    for(int i= 0; i<len; i++){
+        occur[s[i]-'a']++;
+    }
+    
+    for(int i= 0; i<26; i++){
+        if(occur[i] == 0) continue;
+        sum+= occur[i]-1;
+    }
+
+    cout<< sum <<endl;
+}
+
+
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+   
+
+    
+    return 0;
+}

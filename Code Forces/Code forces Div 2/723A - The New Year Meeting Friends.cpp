6b0b258caa1812a14764460ab539009832d8affa
Anonta <anonta9865@gmail.com>
2016-10-03 20:23:01 +0600

    uva and cf entry

---
+++ Code Forces/Code forces Div 2/723A - The New Year Meeting Friends.cpp
@@ -0,0 +1,81 @@
+#Name Anonta Haque #Problm
+#include algorithm
+#include iostream
+#include fstream
+#include sstream
+#include cstring
+#include cstdlib
+#include cstdarg
+#include utility
+#include bitset
+#include cctype
+#include cstdio
+#include vector
+#include string
+#include cmath
+#include ctime
+#include queue
+#include deque
+#include stack
+#include list
+#include set
+#include map
+
+using namespace std;
+
+FOLD ME!!!
+namespace{
+typedef long long LL;
+typedef vectorint vint;
+typedef pairint,int pint;
+typedef mapint,int mapii;
+typedef mapint,bool mapib;
+typedef stringstream strstream;
+typedef unsigned long long ULL;
+
+Macros
+LL CC_;
+#define sf scanf
+#define pf printf
+#define PP getchar();
+#define NL coutn;
+#define pqueue priority_queue
+#define testb(x_, i_) ((x_&1i_)!=0)
+#define setb(x_, i_) (x_=(x_(1i_)))
+#define clrb(x_, i_) (x_=(x_&(!(1i_))))
+#define all(container) container.begin(),container.end()
+#define DC(x_) cout #x_n;DA(x_.begin(), x_.end());
+#define DD(x_) cout( ++CC_ ) #x_ x_endl;
+#define SS printf(_LOOOOOK@MEEEEEEEEEEEEEEE( %d )n,++CC_);
+#define EXT(st_) coutnExicution Time (double)(clock()-st_)CLOCKS_PER_SECendl;
+
+constants
+const int SZ= 1E6;
+const int INF= (129);
+const double EPS= 1E-9;
+const double PI= 2acos(0.0);
+const long long MOD= 1000000007;
+}
+
+void solve(void){
+    int n, co[5];
+    
+    cinco[0]co[1]co[2];
+
+    sort(co, co+3);
+    
+    cout co[1]- co[0] + co[2] - co[1] endl;
+}
+
+
+
+int main(void){
+    ios_basesync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+   
+
+    
+    return 0;
+}

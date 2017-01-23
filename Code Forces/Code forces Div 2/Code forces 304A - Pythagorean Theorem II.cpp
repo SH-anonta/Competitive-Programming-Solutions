428dc0773f3c8d1f0d36bcf59ac11109c82c2884
Anonta <anonta9865@gmail.com>
2017-01-23 14:05:59 +0600

    uva and CF entry

---
+++ Code Forces/Code forces Div 2/Code forces 304A - Pythagorean Theorem II.cpp
@@ -0,0 +1,31 @@
+#include iostream
+#include cmath
+
+using namespace std;
+
+int dumm(int n){
+    int cc= 0;
+    double c;
+    for(int i= 1; i=n; i++){
+        for(int j= i; j=n; j++){
+            c= sqrt(ii + jj);
+            if(c - (int)c == 0 && c = j &&  c = n){
+                cout i     j   c endl;
+                cout c endl;
+                cc++;
+            }
+        }
+    }
+    
+    return cc;
+}
+
+int main(void)
+{
+    int n;
+    cinn;
+    
+    cout dumm(n) endl;
+    
+    return 0;
+}

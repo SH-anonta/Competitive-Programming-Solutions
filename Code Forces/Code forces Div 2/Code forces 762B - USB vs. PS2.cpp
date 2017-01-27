f2093b4e703949274f1735976ad943d69d1d0935
Anonta <anonta9865@gmail.com>
2017-01-28 00:04:11 +0600

    cf entry

---
+++ Code Forces/Code forces Div 2/Code forces 762B - USB vs. PS2.cpp
@@ -0,0 +1,119 @@
+//#Name: Sofen Hoque Anonta  #Problm:
+#include <bits/stdc++.h>
+using namespace std;
+
+//FOLD ME!!!
+namespace{
+typedef long long LL;
+typedef vector<int> vint;
+typedef pair<int,int> pint;
+typedef unsigned long long ULL;
+
+//Macros
+int CC_;
+#define FF first
+#define sf scanf
+#define ZZ second
+#define pf printf
+#define PP cin.get();
+#define NL cout<<endl;
+#define all(container) container.begin(),container.end()
+#define DC(x_) cout<<">>> "<<#x_<<"\n";DA(x_.begin(), x_.end());
+#define DD(x_) cout<<">>>>( "<<++CC_<<" ) "<<#x_<<": "<<x_<<endl;
+#define SS printf(">_<LOOOOOK@MEEEEEEEEEEEEEEE<<( %d )>>\n",++CC_);
+#define EXT(st_) cout<<"\n>>>Exicution Time: "<<(double)(clock()-st_)/CLOCKS_PER_SEC<<endl;
+#define DM(MT,n_,m_)pf("Matrix %s:\n   ", #MT);for(int i_= 0;i_<m_;i_++)pf("%4d ", i_);NL;NL;for(int r_=0;r_<n_;r_++){pf("%2d ", r_);for(int c_= 0;c_<m_;c_++)pf("%4d ", MT[r_][c_]);NL}
+#define mem(a_,b_)(a_, b_, sizeof(a_));
+
+
+//constants
+const double EPS= 1E-9;
+const double PI= 2*acos(0.0);
+const long long MOD= 1000000007;
+
+template <class T>void DA(T i,T e){while(i!=e){cout<<"Con>>( "<<++CC_<<" ) "<<*i++<<endl;}}
+template <class T>void DA(T* x, int l){for(int i=0; i<l;i++)cout<<"["<<i<<"]>> "<<x[i]<<endl;}
+template <class T>inline void sary(T* st, T* nd){while(st<nd)cin>>*st++;/*sf("%d", st++);*/}
+template <class T>void tobin(T n,char*bin){int pos= 1<<((int)log2(n));while(pos>=1){if((n & pos)==0)*bin='0';else*bin='1';pos>>=1;bin++;}*bin='\0';}
+template <class T>int strOccur(string&s,T&tgt){int oc= 0, p= s.find(tgt);while(p != string::npos){p = s.find(tgt, p+1);oc++;}return oc;}
+template <class T>inline T LCM(T x,T y){return((x*y)/__gcd(x,y));}
+LL todec(string& num, int b){LL dec=num[0]-(isupper(num[0])? 'A'-10: '0');for(int i=1;num[i];i++){if(num[i]>='A'&& num[i]<='Z')num[i]-='A'-10;else num[i]-='0';dec*= b;dec+= num[i];}return dec;}
+int bigMod(int b,int e,int m){if(e==0)return 1;if(!(e&1)){int temp=bigMod(b,e/2,m)%m;return(temp*temp)%m;}else return((b%m)*(bigMod(b,e-1,m))%m)%m;}
+inline LL POW(LL base, int exp){LL p= 1; while(exp-->0){p *= base;} return p;}
+bool comp(const int a,const int b){return a>b;}
+}
+
+const int sss= 1E6;
+
+priority_queue<int, vint, greater<int>> ps, us;
+
+void solve(void){
+    int Tc, cost;
+    int n, a,b,c;
+    string type;
+    
+    cin>>a>>b>>c;
+    cin>>n;
+
+    LL total= 0;
+    int cc= 0;
+    
+    for(int i= 0; i<n; i++){
+        cin>>cost>>type;
+        
+        if(type == "USB") us.push(cost);
+        else if(type == "PS/2") ps.push(cost);
+    }
+    
+    for(int i= 0; i<a; i++){
+        if(us.empty()) break;
+        total+= us.top();
+        us.pop();
+        cc++;
+    }
+
+    for(int i= 0; i<b; i++){
+        if(ps.empty()) break;
+        total+= ps.top();
+        ps.pop();
+        cc++;
+    }
+    
+    for(int i= 0; i<c; i++){
+        if(ps.empty() && us.empty()) break;
+        int pp= INT_MAX, uu= INT_MAX;
+        
+        if(!ps.empty()){
+            pp= ps.top();
+        }
+        
+        if(!us.empty()){
+            uu= us.top();
+        }
+        
+        if(pp < uu){
+            total+= pp;
+            ps.pop();
+        }
+        else{
+            total+= uu;
+            us.pop();
+        }
+        
+        cc++;
+    }
+    
+    cout<< cc <<" " << total <<endl;
+}
+
+
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    
+    
+    return 0;
+}
